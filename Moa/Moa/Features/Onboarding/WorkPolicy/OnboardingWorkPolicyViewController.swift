8a594bedec7c5c1f75e68b826c0e70b304d3a630
mirim kim <marisol.develop@gmail.com>
2026-02-06 21:39:36 +0900

    feat: 근무 요일 섹션 구현

--- Moa/Moa/Features/Onboarding/WorkPolicy/OnboardingWorkPolicyViewController.swift
+++ Moa/Moa/Features/Onboarding/WorkPolicy/OnboardingWorkPolicyViewController.swift
@@ -6,13 +6,38 @@
 //
 
 import UIKit
+import SnapKit
 
 final class OnboardingWorkPolicyViewController: BaseViewController {
+    
+    // MARK: - Constants
+    
+    private enum Constant {
+        static let title = "언제 근무하나요?"
+        static let workingHours = "근무 시간"
+        static let lunchBreakTime = "점심 · 휴게 시간"
+    }
     
     // MARK: - Dependencies
     
     private let viewModel: OnboardingWorkPolicyViewModel
     private let onNext: (() -> Void)
+    
+    // MARK: - UI Components
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = Constant.title
+        label.applyTextStyle(.init(
+            typography: AppTypography.t1_700,
+            color: AppColor.IconAndText.highEmphasis
+        ))
+        return label
+    }()
+    
+    private let weekdaySelectionView = WeekdaySelectionView()
+    
+    // MARK: - Init
     
     init(
         viewModel: OnboardingWorkPolicyViewModel,
@@ -27,7 +52,24 @@ final class OnboardingWorkPolicyViewController: BaseViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
+    // MARK: - Setup
+    
     override func setupUI() {
         replaceSystemBackButtonWithAppBackButton()
+        setupLayout()
+    }
+    
+    private func setupLayout() {
+        view.addSubViews([titleLabel, weekdaySelectionView])
+        
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).inset(20)
+            make.leading.trailing.equalTo(view.safeAreaLayoutGuide).inset(AppSpacing.screenHorizontal)
+        }
+        
+        weekdaySelectionView.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(32)
+            make.leading.trailing.equalTo(view.safeAreaLayoutGuide).inset(AppSpacing.screenHorizontal)
+        }
     }
 }
