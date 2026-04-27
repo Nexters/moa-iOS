a86efacf06d5e053c68d513c4916afca2ac6a221
JungDohyeon <jdh1109ok@naver.com>
2026-04-27 21:41:44 +0900

    feat: 오늘 휴가에요 버튼 제거

--- Moa/Moa/Features/Home/Work/WorkMain/Components/WorkMainBottomButtonView.swift
+++ Moa/Moa/Features/Home/Work/WorkMain/Components/WorkMainBottomButtonView.swift
@@ -27,14 +27,8 @@ final class WorkMainBottomButtonView: UIView {
         return button
     }()
 
-    private lazy var vacationButton: UnderlineTextButton = {
-        let button = UnderlineTextButton(title: "오늘 휴가예요")
-        button.addTarget(self, action: #selector(didTapVacation), for: .touchUpInside)
-        return button
-    }()
-
     private lazy var stackView: UIStackView = {
-        let sv = UIStackView(arrangedSubviews: [autoWorkIndicator, primaryButton, vacationButton])
+        let sv = UIStackView(arrangedSubviews: [autoWorkIndicator, primaryButton])
         sv.axis      = .vertical
         sv.alignment = .center
         sv.setCustomSpacing(12, after: autoWorkIndicator)
@@ -76,7 +70,6 @@ final class WorkMainBottomButtonView: UIView {
         self.vacationAction = vacationAction
 
         autoWorkIndicator.isHidden = (status != .idle)
-        vacationButton.isHidden    = !(data.type == .work && status == .idle)
 
         primaryButton.setTitle(data.type.bottomButtonText, for: .normal)
         primaryButton.applyStyle(data.type == .work ? .primary() : .tertiary())
