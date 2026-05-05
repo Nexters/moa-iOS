abba0ac1ff46a672bcaf1bc95dfad9149b6607cb
mirim kim <marisol.develop@gmail.com>
2026-05-05 15:29:22 +0900

    chore: 온보딩 위젯 추가 페이지 back button 커스텀

--- Moa/Moa/Features/Onboarding/Widget/OnboardingAddWidgetViewController.swift
+++ Moa/Moa/Features/Onboarding/Widget/OnboardingAddWidgetViewController.swift
@@ -88,6 +88,7 @@ final class OnboardingAddWidgetViewController: BaseViewController {
     // MARK: - Setup
 
     override func setupUI() {
+        replaceSystemBackButtonWithAppBackButton()
         setupHierarchy()
         setupLayout()
     }
