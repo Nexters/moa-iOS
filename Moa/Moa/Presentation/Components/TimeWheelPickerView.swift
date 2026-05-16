d52daff18f7e5ecd535b5fdfbbc2169b9f308c6b
JungDohyeon <jdh1109ok@naver.com>
2026-05-16 14:01:25 +0900

    fix: 일정 조정 5분 단위로 수정

--- Moa/Moa/Presentation/Components/TimeWheelPickerView.swift
+++ Moa/Moa/Presentation/Components/TimeWheelPickerView.swift
@@ -2,35 +2,44 @@
 //  TimeWheelPickerView.swift
 //  Moa
 //
-//  Created by 정도현 on 2/15/26.
-//
 
 import UIKit
 import SnapKit
 
 // MARK: - TimeWheelPickerViewDelegate
+
 protocol TimeWheelPickerViewDelegate: AnyObject {
     func timeWheelPickerDidChange(_ picker: TimeWheelPickerView, time: TimeIndicatorEntity)
 }
 
 // MARK: - TimeWheelPickerView
+
 final class TimeWheelPickerView: UIView {
     
     // MARK: - Delegate
+    
     weak var delegate: TimeWheelPickerViewDelegate?
     
     // MARK: - Constants
+    
     private let cellHeight: CGFloat = 44
     private let visibleRowCount: CGFloat = 5
+    private let minuteUnit: Int = 5
     
     private var pickerHeight: CGFloat {
         cellHeight * visibleRowCount
     }
+    
     // MARK: - Properties
+    
     private let initialHour: Int
     private let initialMinute: Int
     
+    /// 0, 5, 10, 15 ... 55
+    private let minuteValues = stride(from: 0, through: 55, by: 5).map { $0 }
+    
     // MARK: - Background Container
+    
     private let backgroundContainerView: UIView = {
         let view = UIView()
         view.backgroundColor = AppColor.Container.secondary
@@ -46,25 +55,30 @@ final class TimeWheelPickerView: UIView {
             initialIndex: initialHour,
             alignment: .right
         )
+        
         column.onValueChanged = { [weak self] in
             self?.notifyDelegate()
         }
+        
         return column
     }()
     
     private lazy var minuteColumn: TimeWheelColumnView = {
         let column = TimeWheelColumnView(
-            values: (0...59).map { String(format: "%d분", $0) },
-            initialIndex: initialMinute,
+            values: minuteValues.map { "\($0)분" },
+            initialIndex: minuteIndex(from: initialMinute),
             alignment: .left
         )
+        
         column.onValueChanged = { [weak self] in
             self?.notifyDelegate()
         }
+        
         return column
     }()
     
     // MARK: - Selection Highlight
+    
     private let selectionView: UIView = {
         let view = UIView()
         view.layer.cornerRadius = 12
@@ -72,28 +86,12 @@ final class TimeWheelPickerView: UIView {
         return view
     }()
     
+    // MARK: - Init
+    
     init(initialHour: Int, initialMinute: Int) {
         self.initialHour = initialHour
         self.initialMinute = initialMinute
         super.init(frame: .zero)
-        
-        hourColumn = TimeWheelColumnView(
-            values: (0...23).map { String(format: "%d시", $0) },
-            initialIndex: initialHour,
-            alignment: .right
-        )
-        hourColumn.onValueChanged = { [weak self] in
-            self?.notifyDelegate()
-        }
-        
-        minuteColumn = TimeWheelColumnView(
-            values: (0...59).map { String(format: "%d분", $0) },
-            initialIndex: initialMinute,
-            alignment: .left
-        )
-        minuteColumn.onValueChanged = { [weak self] in
-            self?.notifyDelegate()
-        }
         
         setupLayout()
     }
@@ -103,28 +101,44 @@ final class TimeWheelPickerView: UIView {
     }
     
     // MARK: - Public Methods
+    
     var selectedTime: TimeIndicatorEntity {
         TimeIndicatorEntity.from(
             hour: hourColumn.selectedIndex,
-            minute: minuteColumn.selectedIndex
+            minute: selectedMinuteValue
         )
     }
     
     func setTime(hour: Int, minute: Int, animated: Bool = false) {
         hourColumn.scrollToIndexSilently(hour, animated: animated)
-        minuteColumn.scrollToIndexSilently(minute, animated: animated)
+        
+        let minuteIndex = minuteIndex(from: minute)
+        minuteColumn.scrollToIndexSilently(minuteIndex, animated: animated)
     }
     
     // MARK: - Private Methods
+    
+    private var selectedMinuteValue: Int {
+        minuteValues[minuteColumn.selectedIndex]
+    }
+    
+    private func minuteIndex(from minute: Int) -> Int {
+        let roundedMinute = (minute / minuteUnit) * minuteUnit
+        return minuteValues.firstIndex(of: roundedMinute) ?? 0
+    }
+    
     private func notifyDelegate() {
         delegate?.timeWheelPickerDidChange(self, time: selectedTime)
     }
 }
 
+// MARK: - Layout
+
 private extension TimeWheelPickerView {
     
     func setupLayout() {
         addSubview(backgroundContainerView)
+        
         backgroundContainerView.addSubview(selectionView)
         backgroundContainerView.addSubview(hourColumn)
         backgroundContainerView.addSubview(minuteColumn)
